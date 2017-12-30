8bb180847fb6e36cbc0afd1030fc232ca169b950
I3EAST <asidhu@ucdavis.edu>
2017-12-29 16:05:44 -0800

    Working Still

--- server/awsSetup.js
+++ server/awsSetup.js
@@ -1,38 +1,53 @@
 const AWS = require('aws-sdk');
 AWS.config.loadFromPath('./config/staging.json');
+const Consumer = require('sqs-consumer');
 
-const sqs = new AWS.SQS();
-
-
-const params = {
-  AttributeNames: [
-    'SentTimestamp',
-  ],
-  MaxNumberOfMessages: 1,
-  MessageAttributeNames: [
-    'All',
-  ],
-  QueueUrl: 'https://sqs.us-west-1.amazonaws.com/016977445519/IncecntiveProduct',
-  VisibilityTimeout: 0,
-  WaitTimeSeconds: 0,
-};
+// const sqs = new AWS.SQS();
 
 
-sqs.receiveMessage(params, (err, data) => {
-  if (err) {
-    console.log("Receive Error", err);
-  } else if (data.Messages) {
-    console.log(data.Messages);
-    var deleteParams = {
-      QueueUrl: 'https://sqs.us-west-1.amazonaws.com/016977445519/IncecntiveProduct',
-      ReceiptHandle: data.Messages[0].ReceiptHandle
-    };
-    sqs.deleteMessage(deleteParams, function(err, data) {
-      if (err) {
-        console.log("Delete Error", err);
-      } else {
-        console.log("Message Deleted", data);
-      }
-    });
-  }
+// const params = {
+//   AttributeNames: [
+//     'SentTimestamp',
+//   ],
+//   MaxNumberOfMessages: 1,
+//   MessageAttributeNames: [
+//     'All',
+//   ],
+//   QueueUrl: 'https://sqs.us-west-1.amazonaws.com/016977445519/IncecntiveProduct',
+//   VisibilityTimeout: 0,
+//   WaitTimeSeconds: 0,
+// };
+
+
+// sqs.receiveMessage(params, (err, data) => {
+//   if (err) {
+//     console.log("Receive Error", err);
+//   } else if (data.Messages) {
+//     console.log(data.Messages);
+//     var deleteParams = {
+//       QueueUrl: 'https://sqs.us-west-1.amazonaws.com/016977445519/IncecntiveProduct',
+//       ReceiptHandle: data.Messages[0].ReceiptHandle
+//     };
+//     sqs.deleteMessage(deleteParams, function(err, data) {
+//       if (err) {
+//         console.log("Delete Error", err);
+//       } else {
+//         console.log("Message Deleted", data);
+//       }
+//     });
+//   }
+// });
+
+const app = Consumer.create({
+  queueUrl: 'https://sqs.us-west-1.amazonaws.com/016977445519/IncecntiveProduct',
+  handleMessage: (message, done) => {
+    console.log(message);
+    done();
+  },
+  sqs: new AWS.SQS(),
 });
+
+app.on('error', (err) => {
+  console.log(err.message);
+});
+app.start();
